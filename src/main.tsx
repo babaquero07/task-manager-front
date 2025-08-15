cfbd8e208276012cfd602345e03f7774b98cb6fe
Alexander Baquero <brayanalexanderb@gmail.com>
2025-08-15 11:50:53 -0500

    Refactor App component and update routing; replace App with Tasks in main.tsx, and integrate Navbar and Footer in TasksLayout

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import TasksLayout from "./ui/layout/TasksLayout.tsx";
-import App from "./App.tsx";
+import Tasks from "./ui/pages/Tasks.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<TasksLayout />}>
           <Route path="/" element={<Navigate to="/tasks" />} />
-          <Route path="/tasks" element={<App />} />
+          <Route path="/tasks" element={<Tasks />} />
         </Route>
       </Routes>
     </BrowserRouter>
