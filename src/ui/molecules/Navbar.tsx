6b3503e77b67e6166ddb52399f463e565bf86baa
Alexander Baquero <brayanalexanderb@gmail.com>
2025-08-15 13:41:01 -0500

    Update Navbar component to use Link for navigation and enhance button functionality for task creation

--- src/ui/molecules/Navbar.tsx
+++ src/ui/molecules/Navbar.tsx
@@ -1,8 +1,17 @@
+import { Link, useNavigate } from "react-router";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="flex justify-between items-center p-4 border-b border-gray-200">
-      <p className="text-2xl font-bold">Task Manager 🖊️</p>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+      <Link to="/tasks" className="text-2xl font-bold">
+        Task Manager 🖊️
+      </Link>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-blue-600 transition-colors"
+        onClick={() => navigate("/tasks/create")}
+      >
         Crear tarea
       </button>
     </nav>
